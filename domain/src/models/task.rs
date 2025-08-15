18fa4a7b5aefbd0e2be3463a7bc035fc09444d74
Jan Żukowski <janzukowski@proton.me>
2025-08-15 17:32:19 +0200

    util: remodelled the Task model as it wasn't considered enough

--- domain/src/models/task.rs
+++ domain/src/models/task.rs
@@ -1,22 +1,14 @@
 #[derive(serde::Serialize, serde::Deserialize)]
-pub struct UncompletedTask {
+#[serde(untagged)]
+pub enum TaskCompletion {
+    Completed,
+    Uncompleted
+}
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Task {
     pub id: i32,
     pub title: String,
-    pub description: String
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-pub struct CompletedTask {
-    pub id: i32,
-    pub title: String
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-#[serde(tag = "completion")]
-pub enum Task {
-    #[serde(rename = "completed")]
-    Uncompleted(UncompletedTask),
-
-    #[serde(rename = "uncompleted")]
-    Completed(CompletedTask),
+    pub description: String,
+    pub completion: TaskCompletion
 }
