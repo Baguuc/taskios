b707732ccd97cdd1e7bdfd695b31acc403498409
Jan Żukowski <janzukowski@proton.me>
2025-10-25 19:22:13 +0200

    chore: secured project update feature with resource permission

--- src/features/project/update.rs
+++ src/features/project/update.rs
@@ -7,8 +7,14 @@ impl ProjectUpdateFeature {
         authios_client: authios_sdk::AuthiosClient
     ) -> Result<(), crate::errors::feature::ProjectUpdateError> {
         use crate::errors::feature::ProjectUpdateError as Error;
-        use authios_sdk::requests::LoggedUserCheckServicePermissionRequest as AuthRequest;
-        use authios_sdk::responses::LoggedUserCheckServicePermissionResponse as AuthResponse;
+        use authios_sdk::requests::{
+            LoggedUserCheckServicePermissionRequest as ServicePermissionRequest,
+            LoggedUserCheckResourcePermissionRequest as ResourcePermissionRequest
+        };
+        use authios_sdk::responses::{
+            LoggedUserCheckServicePermissionResponse as ServicePermissionResponse,
+            LoggedUserCheckResourcePermissionResponse as ResourcePermissionResponse
+        };
         
         if params.new_name.is_none() {
             return Ok(());
@@ -18,24 +24,47 @@ impl ProjectUpdateFeature {
             .await
             .unwrap();
 
-        let auth_response = authios_client.query()
+        let service_permission_response = authios_client.query()
             .user()
             .logged(params.token.clone())
             .permissions()
             .service()
-            .check(AuthRequest {
+            .check(ServicePermissionRequest {
                 service_id: String::from("taskios")
             })
             .await;
 
-        match auth_response {
-            AuthResponse::Ok { has_permission } => if !has_permission {
+        match service_permission_response {
+            ServicePermissionResponse::Ok { has_permission } => if !has_permission {
                 return Err(Error::Unauthorized)
             },
-            AuthResponse::InvalidToken => return Err(Error::InvalidToken),
-            AuthResponse::ServerNotAuthios => panic!("AUTH SERVER ERROR: auth server returns invalid responses"),
-            AuthResponse::ServerUnavailable => panic!("AUTH SERVER ERROR: auth server shut down"),
-            AuthResponse::PermissionNotFound => panic!("AUTH SERVER ERROR: auth server wasn't inited - it's lacking crucial permissions to run this software")
+            ServicePermissionResponse::InvalidToken => return Err(Error::InvalidToken),
+            ServicePermissionResponse::ServerNotAuthios => panic!("AUTH SERVER ERROR: auth server returns invalid responses"),
+            ServicePermissionResponse::ServerUnavailable => panic!("AUTH SERVER ERROR: auth server shut down"),
+            ServicePermissionResponse::PermissionNotFound => panic!("AUTH SERVER ERROR: auth server wasn't inited - it's lacking crucial permissions to run this software")
+        };
+        
+        let resource_permission_response = authios_client.query()
+            .user()
+            .logged(params.token.clone())
+            .permissions()
+            .resource()
+            .check(ResourcePermissionRequest {
+                service_id: String::from("taskios"),
+                resource_type: String::from("vault"),
+                resource_id: params.id.to_string(),
+                permission_name: String::from("manage"),
+            })
+            .await;
+
+        match resource_permission_response {
+            ResourcePermissionResponse::Ok { has_permission } => if !has_permission {
+                return Err(Error::Unauthorized)
+            },
+            ResourcePermissionResponse::InvalidToken => return Err(Error::InvalidToken),
+            ResourcePermissionResponse::ServerNotAuthios => panic!("AUTH SERVER ERROR: auth server returns invalid responses"),
+            ResourcePermissionResponse::ServerUnavailable => panic!("AUTH SERVER ERROR: auth server shut down"),
+            ResourcePermissionResponse::PermissionNotFound => panic!("AUTH SERVER ERROR: auth server wasn't inited - it's lacking crucial permissions to run this software")
         };
         
         let sql = "UPDATE projects SET name = $1 WHERE id = $2;";
